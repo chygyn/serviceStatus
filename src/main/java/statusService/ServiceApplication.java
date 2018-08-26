2bb495d3eb99ea787dfc0c58be7d4c368bdb7f47
Egor <e.pavlov@evola.ru>
2018-08-26 18:12:17 +0300

    Deploy

--- src/main/java/statusService/ServiceApplication.java
+++ src/main/java/statusService/ServiceApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceApplication {
+    String s="";
 
     public static void main(String[] args) {
+
         SpringApplication.run(ServiceApplication.class, args);
     }
 }
